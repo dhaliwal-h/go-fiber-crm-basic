282801162f3e5fe9c549e846d229af0a7e71327a
Harsimranjit Dhaliwal <dhaliwal-h@outlook.com>
2022-07-16 13:55:16 -0400

    Basic setup done

--- lead/lead.go
+++ lead/lead.go
@@ -1,1 +1,19 @@
 package lead
+
+import (
+	"github.com/gofiber/fiber"
+	"github.com/jinzhu/gorm"
+)
+
+type Lead struct {
+	gorm.Model
+	Name    string
+	Company string
+	Email   string
+	Phone   int
+}
+
+func GetLead(ctx *fiber.Ctx)    {}
+func GetLeads(ctx *fiber.Ctx)   {}
+func NewLead(ctx *fiber.Ctx)    {}
+func DeleteLead(ctx *fiber.Ctx) {}
--- main.go
+++ main.go
@@ -1,20 +1,43 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 
+	"github.com/dhaliwal-h/go-fiber-crm-basic/database"
+	"github.com/dhaliwal-h/go-fiber-crm-basic/lead"
 	"github.com/gofiber/fiber"
+	"github.com/jinzhu/gorm"
 )
 
 func setUpRoutes(app *fiber.App) {
-	app.Get(GetLeads)
-	app.Get(GetLead)
-	app.Post(NewLead)
-	app.Delete(DeleteLead)
+	app.Get("/api/v1/lead", lead.GetLeads)
+	app.Get("/api/v1/lead/:id", lead.GetLead)
+	app.Post("/api/v1/lead", lead.NewLead)
+	app.Delete("/api/v1/lead/:id", lead.DeleteLead)
+	app.Get("/", func(c *fiber.Ctx) {
+		c.Fasthttp.Response.Header.Set("Content-Type", "application/json")
+
+		json.NewEncoder(c.Fasthttp.Response.BodyWriter()).Encode("Hello World.")
+	})
 }
 
+func initDatabase() {
+	var err error
+	database.DBConn, err = gorm.Open("sqlite3", "lead.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connection opened to database")
+	database.DBConn.AutoMigrate(&lead.Lead{})
+	fmt.Println("Database Migrated")
+}
 func main() {
 	fmt.Println("Hello world")
+	initDatabase()
 	app := fiber.New()
 	setUpRoutes(app)
+	app.Listen(3000)
+	defer database.DBConn.Close()
 }
